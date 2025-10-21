c5d204699ecda48d8a8f177929b5a64cd2314f8b
Nicolas Klutchnikoff <nicolas.klutchnikoff@gmail.com>
2025-10-21 22:17:37 +0200

    fix: doc config with sphinx

---
+++ docs/conf.py
@@ -0,0 +1,12 @@
+# Minimal Sphinx configuration for ReadTheDocs
+# Note: This project uses Quarto for documentation generation
+# This file is only required to satisfy ReadTheDocs v2 requirements
+
+project = 'kmeanssa-ng'
+author = 'Your Name'
+release = '0.2.3'
+
+extensions = []
+templates_path = []
+exclude_patterns = []
+html_theme = 'alabaster'
